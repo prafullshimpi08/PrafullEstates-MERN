3ef8c6bc68ba60af48c05295a416409e6f0a1389
prafull <prafullshimpi2829@.com>
2025-03-08 12:14:13 +0530

    ready to work

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Prafull's Estate</h1>
-      <p className='mb-4 text-slate-700'>Prafull's Estate is a leading real estate agency specializing in helping clients buy, sell, and rent properties in prime locations. Our team of experienced professionals is committed to providing exceptional service, ensuring a seamless and hassle-free property transaction experience. With a focus on customer satisfaction and market expertise, Prafull's Estate is your trusted partner in finding the perfect home or investment opportunity.</p>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Prafull Estate</h1>
+      <p className='mb-4 text-slate-700'>Prafull Estate is a leading real estate agency specializing in helping clients buy, sell, and rent properties in prime locations. Our team of experienced professionals is committed to providing exceptional service, ensuring a seamless and hassle-free property transaction experience. With a focus on customer satisfaction and market expertise, Prafull Estate is your trusted partner in finding the perfect home or investment opportunity.</p>
       <p className='mb-4 text-slate-700'>
-      At Prafull's Estate, our mission is to help clients achieve their real estate goals with expert advice, personalized service, and in-depth knowledge of the local market. Whether you're looking to buy, sell, or rent a property, our dedicated team is here to guide you through every step of the process, ensuring a smooth and successful experience.
+      At Prafull Estate, our mission is to help clients achieve their real estate goals with expert advice, personalized service, and in-depth knowledge of the local market. Whether you're looking to buy, sell, or rent a property, our dedicated team is here to guide you through every step of the process, ensuring a smooth and successful experience.
       </p>
-      <p className='mb-4 text-slate-700'>The team at Prafull's Estate brings extensive experience and deep industry knowledge to the real estate market. We are committed to delivering the highest level of service, ensuring that every client’s journey—whether buying, selling, or renting—is smooth, exciting, and rewarding. Our goal is to turn your real estate aspirations into reality with professionalism, integrity, and a customer-first approach.</p>
+      <p className='mb-4 text-slate-700'>The team at Prafull Estate brings extensive experience and deep industry knowledge to the real estate market. We are committed to delivering the highest level of service, ensuring that every client’s journey—whether buying, selling, or renting—is smooth, exciting, and rewarding. Our goal is to turn your real estate aspirations into reality with professionalism, integrity, and a customer-first approach.</p>
     </div>
   )
 }
